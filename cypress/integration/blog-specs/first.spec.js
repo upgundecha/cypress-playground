e3efbbfd01b6d5edd38fd56381d9af4330740ce0
Unmesh Gundecha <upgundecha@gmail.com>
2020-01-07 22:00:42 +0800

    First test

---
+++ cypress/integration/blog-specs/first.spec.js
@@ -0,0 +1,9 @@
+describe('Heading text', () => {
+    it('contains the correct title', ()=> {
+        cy.visit('https://unmesh.me/');
+
+        cy.get('h1.site-title a')
+            .invoke('text')
+            .should('equal', 'TestO\'Matic');
+    })
+})
