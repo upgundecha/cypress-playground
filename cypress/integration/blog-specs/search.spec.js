1ec15e2ef107853054e337436b0d955abdc17435
Unmesh Gundecha <upgundecha@gmail.com>
2020-01-09 23:09:07 +0800

    Search test

--- cypress/integration/blog-specs/search.spec.js
+++ cypress/integration/blog-specs/search.spec.js
@@ -2,5 +2,9 @@ describe('Search ', () => {
     it('return searched item', ()=> {
         cy.visit('https://unmesh.me/');
         cy.get('input.search-field').type('jagacy{enter}');
+        cy.get('article header h1 a')
+            .invoke('text')
+            .should('equal', 'Automated Acceptance testing for Mainframe with Cucumber and Jagacy');
+            
     })
 })
