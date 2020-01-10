0fee011631578653654ea13605730bfa4dd49764
Unmesh Gundecha <upgundecha@gmail.com>
2020-01-10 21:40:36 +0800

    Navigation tests

--- cypress/integration/blog-specs/navigation.spec.js
+++ cypress/integration/blog-specs/navigation.spec.js
@@ -1,4 +1,5 @@
 describe('Navigation ', () => {
+    
     it('contains the About link', ()=> {
         cy.visit('https://unmesh.me/');
 
@@ -9,4 +10,27 @@ describe('Navigation ', () => {
             cy.title()
             .should('equal', 'About – TestO\'Matic')
     })
+
+    it('contains the Resources', ()=> {
+        cy.visit('https://unmesh.me/');
+
+        cy.get('a')
+            .contains('Resources')
+            .click();
+        
+            cy.title()
+            .should('equal', 'Resources – TestO\'Matic')
+    })
+
+    it('contains the QTP Cookbook', ()=> {
+        cy.visit('https://unmesh.me/');
+
+        cy.get('a')
+            .contains('QTP Cookbook')
+            .click();
+        
+            cy.title()
+            .should('equal', 'QTP Cookbook – TestO\'Matic')
+    })
+
 })
