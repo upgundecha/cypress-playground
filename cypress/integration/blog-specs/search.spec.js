4bdb561af3ec02cc1e9b8ccea264095002930449
Unmesh Gundecha <upgundecha@gmail.com>
2020-01-14 22:33:51 +0800

    Fix

--- cypress/integration/blog-specs/search.spec.js
+++ cypress/integration/blog-specs/search.spec.js
@@ -1,6 +1,7 @@
 describe('Search ', () => {
     it('return searched item', ()=> {
-
+        cy.visit('https://unmesh.me/');
+        cy.get('input.search-field').should('have.attr','placeholder', 'Search …')
         cy.get('input.search-field').type('jagacy{enter}');
         cy.get('article header h1 a')
             .invoke('text')
