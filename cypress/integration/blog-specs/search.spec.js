3980eff737c91deea462365cecd5cb92308fc92e
Unmesh Gundecha <upgundecha@gmail.com>
2020-01-15 21:34:27 +0800

    Fix failing test

--- cypress/integration/blog-specs/search.spec.js
+++ cypress/integration/blog-specs/search.spec.js
@@ -2,9 +2,9 @@ describe('Search ', () => {
     it('return searched item', ()=> {
         cy.visit('https://unmesh.me/');
         cy.get('input.search-field').should('have.attr','placeholder', 'Search …')
-        cy.get('input.search-field').type('jagacy{enter}');
-        cy.get('article header h1 a')
+        cy.get('input.search-field').type('faker{enter}');
+        cy.get('article header h1')
             .invoke('text')
-            .should('equal', 'Automated Acceptance testing for Mainframe with Cucumber and Jagacy');
+            .should('contain', 'Generating Test Data with Faker');
     })
 })
